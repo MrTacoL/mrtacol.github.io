b2b9cd4a65c247c66db1174b92d0045e34cc5159
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 18:33:45 -0400

    Add profile config for Discord and music

---
+++ config.js
@@ -0,0 +1,12 @@
+window.MRTACOSI_CONFIG = {
+  // Put your numeric Discord user ID here when you want it public for everyone.
+  // Example: discordUserId: "123456789012345678"
+  discordUserId: "",
+
+  // After you upload your song to /assets/music.mp3, the button will play it.
+  musicSrc: "./assets/music.mp3",
+
+  profileName: "mrtacosi",
+  fallbackActivity: "Playing Code",
+  fallbackSubline: "Idling"
+};
