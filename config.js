5cb694335328653bb5319f06eb310377d52a88cc
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 18:40:57 -0400

    Set Discord user ID

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 window.MRTACOSI_CONFIG = {
-  // Put your numeric Discord user ID here when you want it public for everyone.
-  // Example: discordUserId: "123456789012345678"
-  discordUserId: "",
-
-  // After you upload your song to /assets/music.mp3, the button will play it.
+  discordUserId: "828806896089038879",
   musicSrc: "./assets/music.mp3",
-
   profileName: "mrtacosi",
   fallbackActivity: "Playing Code",
   fallbackSubline: "Idling"
