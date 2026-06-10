9196ff7447840f4c27802babcec3f6c8466c097b
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 19:55:45 -0400

    Bust music cache and show Discord loading fallback

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.MRTACOSI_CONFIG = {
   discordUserId: "828806896089038879",
-  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3",
+  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3?v=64kbps-2",
   profileName: "mrtacosi",
-  fallbackActivity: "Playing Code",
-  fallbackSubline: "Idling"
+  fallbackActivity: "Loading Discord",
+  fallbackSubline: "Checking live status"
 };
