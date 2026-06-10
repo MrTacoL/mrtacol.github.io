a910b9100f83efa02b81d313c29e6ac4accf0f9a
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 19:28:14 -0400

    Link uploaded PLAZA music video as audio source

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MRTACOSI_CONFIG = {
   discordUserId: "828806896089038879",
-  musicSrc: "./assets/music.mp3",
+  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20(1).mp4",
   profileName: "mrtacosi",
   fallbackActivity: "Playing Code",
   fallbackSubline: "Idling"
