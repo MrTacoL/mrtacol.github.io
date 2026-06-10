66059779771e0476df0ede1b80739e44c849c319
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 19:50:50 -0400

    Link 64 Kbps MP3 music source

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MRTACOSI_CONFIG = {
   discordUserId: "828806896089038879",
-  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20(1).mp4",
+  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3",
   profileName: "mrtacosi",
   fallbackActivity: "Playing Code",
   fallbackSubline: "Idling"
