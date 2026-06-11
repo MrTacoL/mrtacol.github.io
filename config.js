5b68ba4ab825213c8e1229b0e1c99908d2c8ce7e
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 20:00:47 -0400

    Match instant audio cache source

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MRTACOSI_CONFIG = {
   discordUserId: "828806896089038879",
-  musicSrc: "./assets/PLAZA - Personal (Official Music Video) - (64 Kbps).mp3?v=64kbps-3",
+  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3?v=64kbps-4",
   profileName: "mrtacosi",
   fallbackActivity: "Loading Discord",
   fallbackSubline: "Checking live status"
