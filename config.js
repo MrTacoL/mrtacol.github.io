53293ff24caad76c83d8768b8b28a39e07f95c3d
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 20:29:38 -0400

    Bump MP3 cache for instant priming

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MRTACOSI_CONFIG = {
   discordUserId: "828806896089038879",
-  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3?v=64kbps-4",
+  musicSrc: "./assets/PLAZA%20-%20Personal%20(Official%20Music%20Video)%20-%20(64%20Kbps).mp3?v=64kbps-5",
   profileName: "mrtacosi",
   fallbackActivity: "Online",
   fallbackSubline: "Active now"
