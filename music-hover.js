1b0e3574fe6f5e8ff5f23835f6b8f77d7e97e9b3
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 21:50:32 -0400

    Update site

--- music-hover.js
+++ music-hover.js
@@ -3,20 +3,22 @@
   const frame = document.getElementById('musicPreviewFrame');
   if (!hover || !frame) return;
 
-  const videoSrc = frame.dataset.src;
+  const videoId = 'EUshgvt7I8U';
+  const videoSrc = `https://www.youtube-nocookie.com/embed/${videoId}?start=182&controls=0&mute=1&modestbranding=1&playsinline=1`;
+  frame.dataset.src = videoSrc;
   let closeTimer;
 
   function showPreview() {
     clearTimeout(closeTimer);
     hover.classList.add('is-hovering');
-    if (videoSrc && frame.src !== videoSrc) frame.src = videoSrc;
+    if (frame.src !== videoSrc) frame.src = videoSrc;
   }
 
   function hidePreview() {
     closeTimer = setTimeout(() => {
       hover.classList.remove('is-hovering');
       frame.removeAttribute('src');
-    }, 120);
+    }, 160);
   }
 
   hover.addEventListener('mouseenter', showPreview);
