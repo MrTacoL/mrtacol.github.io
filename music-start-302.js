a5951268648883540050d5ac5ee4e3c373cbb70e
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-10 21:50:16 -0400

    Update site

---
+++ music-start-302.js
@@ -0,0 +1,5 @@
+(() => {
+  const frame = document.getElementById('musicPreviewFrame');
+  if (!frame) return;
+  frame.dataset.src = 'https://www.youtube-nocookie.com/embed/EUshgvt7I8U?start=182&controls=0&mute=1&modestbranding=1&playsinline=1';
+})();
